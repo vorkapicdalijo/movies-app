3cf861ec8a5a5af899505a4bd3f130b5195f8323
Dalijo Vorkapić <dalijo.vorkapic@ericsson.com>
2023-11-28 23:39:17 +0100

    added get and query for the list of movies

--- backend/movies-app/src/main/java/com/xpandit/moviesapp/dao/JdbcMoviesDAO.java
+++ backend/movies-app/src/main/java/com/xpandit/moviesapp/dao/JdbcMoviesDAO.java
@@ -4,6 +4,9 @@ import com.xpandit.moviesapp.interfaces.IMoviesDAO;
 import com.xpandit.moviesapp.model.Movie;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,15 +16,69 @@ public class JdbcMoviesDAO implements IMoviesDAO {
 
     private final Logger log = LogManager.getLogger(JdbcMoviesDAO.class);
 
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcMoviesDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     // MOVIE DAO - START
     @Override
     public List<Movie> getMovies() {
-        return null;
+        try {
+            final StringBuilder queryBuilder = new StringBuilder()
+                    .append(" SELECT ")
+                    .append("     m.id, ")
+                    .append("     m.title, ")
+                    .append("     m.description, ")
+                    .append("     m.duration, ")
+                    .append("     m.release_date, ")
+                    .append("     m.revenue, ")
+                    .append("     mt.type_id, ")
+                    .append("     mt.type_name ")
+                    .append(" FROM ")
+                    .append("     movies m ")
+                    .append(" LEFT JOIN ")
+                    .append("     movie_types mt ")
+                    .append(" ON ")
+                    .append("     m.movie_type_id = mt.type_id ");
+
+            List<Movie> movies = jdbcTemplate.query(queryBuilder.toString(), (resultSet, i) -> {
+                Movie movie = new Movie();
+
+                movie.setId(Integer.parseInt(resultSet.getString("id")));
+                movie.setMovieTypeName(resultSet.getString("type_name"));
+                movie.setMovieTypeId(Integer.parseInt(resultSet.getString("type_id")));
+                movie.setTitle(resultSet.getString("title"));
+                movie.setDescription(resultSet.getString("description"));
+                movie.setReleaseDate(resultSet.getDate("release_date"));
+                movie.setDuration(resultSet.getTime("duration"));
+                movie.setRevenue(Float.parseFloat(resultSet.getString("revenue")));
+
+                return movie;
+            });
+            return movies;
+
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+        catch (Exception e) {
+            log.error("Error while fetching movies: {}", e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public Movie getMovieDetailsById(Integer movieId) {
+        if (movieId == null) {
+            throw new IllegalArgumentException("Missing movie ID parameter!");
+        }
+        try {
+
+        } catch (Exception e) {
+            log.error("Error while fetching movie details: {}", e.getMessage());
+        }
         return null;
     }
 
