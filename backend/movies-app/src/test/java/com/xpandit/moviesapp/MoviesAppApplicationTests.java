23893c4bc84e909baf4f5d9d365307cc31844a01
Dalijo Vorkapić <dalijo.vorkapic@ericsson.com>
2023-11-28 13:18:11 +0100

    init

---
+++ backend/movies-app/src/test/java/com/xpandit/moviesapp/MoviesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpandit.moviesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
