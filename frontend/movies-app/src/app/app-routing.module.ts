067b145d5f755862029f1e0f05396f6dde54917a
Dalijo Vorkapić <dalijo.vorkapic@ericsson.com>
2023-12-03 10:54:33 +0100

    routing, modules, env

--- frontend/movies-app/src/app/app-routing.module.ts
+++ frontend/movies-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesComponent } from './pages/movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/movies', pathMatch: 'full' },
+  { path: 'movies', component: MoviesComponent },
+  { path: '**', redirectTo: '/movies' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/movies-app/src/app/app.module.ts
+++ frontend/movies-app/src/app/app.module.ts
@@ -1,18 +1,38 @@
+import { MoviesComponent } from './pages/movies/movies.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MoviesService } from './services/movies.service';
+import { HeaderComponent } from './components/header/header.component';
+import { MatRippleModule } from '@angular/material/core';
+import { DurationPipe } from './components/pipes/time-duration.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    HeaderComponent,
+    DurationPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatRippleModule
   ],
-  providers: [],
+  providers: [MoviesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
