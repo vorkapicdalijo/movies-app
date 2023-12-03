3aa8620b81bd31e427baf80fc466215e9b565382
Dalijo Vorkapić <dalijo.vorkapic@ericsson.com>
2023-12-03 10:50:56 +0100

    cors setup

---
+++ backend/movies-app/src/main/java/com/xpandit/moviesapp/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.xpandit.moviesapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow all origins, headers, and methods for simplicity. You may want to configure this based on your needs.
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
